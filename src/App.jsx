02e58caddd5e410ea6649cf80b29e35ac19ca3b3
Nitin Baranwal <nitinkumarbaranawal@gmail.com>
2026-02-01 16:42:39 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,41 @@ import './App.css'
 import Container from "./Components/Container";
 import { useState } from "react";
 function App() {
-  let savedList=localStorage.getItem("MyList")||[];
-  let [originalList,setList]=useState(JSON.parse(savedList));
-  
-  
-  let addElement=(text,date)=>{
-    let newObj = {text,date};
-    let list = [...originalList,newObj];
-    console.log(originalList);
-    localStorage.setItem("MyList",JSON.stringify(list));
+  let savedList = localStorage.getItem("MyList");
+  let [originalList, setList] = useState(
+    savedList ? JSON.parse(savedList) : []   // fallback to empty array
+  );
+
+  let addElement = (text, date) => {
+    let newObj = { text, date };
+    let list = [...originalList, newObj];
+    localStorage.setItem("MyList", JSON.stringify(list));
     setList(list);
   };
-  let delElement=(text,date)=>{
-    let index=originalList.findIndex(item=>(item.text===text&&item.date===date));
-    let newArr=[...originalList];
-    newArr.splice(index,1);
-    localStorage.setItem("MyList",JSON.stringify(newArr));
+
+  let delElement = (text, date) => {
+    let index = originalList.findIndex(
+      (item) => item.text === text && item.date === date
+    );
+    let newArr = [...originalList];
+    if (index !== -1) newArr.splice(index, 1);
+    localStorage.setItem("MyList", JSON.stringify(newArr));
     setList(newArr);
   };
-  return (<Container>
-            <Heading/>
-            <Input addElement={addElement}></Input>
-            {originalList.map((item,index)=>(
-              <Item1 key={index} text={item.text} date={item.date} delElement={delElement}></Item1>
-            ))}
-            
-          </Container>
+
+  return (
+    <Container>
+      <Heading />
+      <Input addElement={addElement} />
+      {originalList.map((item, index) => (
+        <Item1
+          key={index}
+          text={item.text}
+          date={item.date}
+          delElement={delElement}
+        />
+      ))}
+    </Container>
   );
 }
 
